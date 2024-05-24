4c6df975dfb315d419fd3c40ff3ab38bdb665ed8
Tiger <tagir.tsarkov@gmail.com>
2024-05-24 08:17:11 +0300

    poistin oudon mostalignednormal logiikan fysiikka systeemistä mikä korjasi ongelmia

--- sandboxes/sandbox1/level1.cpp
+++ sandboxes/sandbox1/level1.cpp
@@ -13,13 +13,13 @@ void Level1::onStart()
 	camera->createComponent<TransformComponent>(glm::vec3(-320.0f, -240.0f, 0), glm::vec3(0, 0, 0), glm::vec3(1, 1, 1));
 
 	Level1Factory levelFactory;
-	levelFactory.buildBox(this, 2000, glm::vec3(-100, 20, 0), glm::vec3(0, 0, 20), glm::vec3(50, 50, 1));
-	levelFactory.buildBox(this, 2000, glm::vec3(100, 20, 0), glm::vec3(0, 0, 0), glm::vec3(50, 50, 1));
-	levelFactory.buildBox(this, 7000, glm::vec3(100, 200, 0), glm::vec3(0, 0, 0), glm::vec3(60, 60, 1));
-	levelFactory.buildBox(this, 600, glm::vec3(0, 100, 0), glm::vec3(0, 0, 0), glm::vec3(300, 20, 1));
-	levelFactory.buildBox(this, 100, glm::vec3(-20, 200, 0), glm::vec3(0, 0, 80), glm::vec3(100, 15, 1));
+	//levelFactory.buildBox(this, 2000, glm::vec3(-100, 20, 0), glm::vec3(0, 0, 0), glm::vec3(50, 50, 1));
+	//levelFactory.buildBox(this, 2000, glm::vec3(100, 20, 0), glm::vec3(0, 0, 0), glm::vec3(50, 50, 1));
+	//levelFactory.buildBox(this, 7000, glm::vec3(100, 200, 0), glm::vec3(0, 0, 0), glm::vec3(60, 60, 1));
+	//levelFactory.buildBox(this, 600, glm::vec3(0, 100, 0), glm::vec3(0, 0, 0), glm::vec3(300, 20, 1));
+	//levelFactory.buildBox(this, 100, glm::vec3(-20, 200, 0), glm::vec3(0, 0, 80), glm::vec3(100, 15, 1));
 	levelFactory.buildGround(this, 10000000000000, glm::vec3(0, -90, 0), glm::vec3(0, 0, 0), glm::vec3(400, 40, 1));
-	//levelFactory.buildPlayer(this, glm::vec3(0, 0, 0), glm::vec3(0, 0, 0), glm::vec3(50, 50, 1));
+	levelFactory.buildPlayer(this, glm::vec3(0, 0, 0), glm::vec3(0, 0, 0), glm::vec3(50, 50, 1));
 
 
 	newSystem<PlayerBehavior>(ANIMATION_FRAME_TIME);
